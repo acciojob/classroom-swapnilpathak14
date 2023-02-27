c44f2d72364489905c3babbbfcdf815511166d10
Swapnil Pathak <61601014+swapnilpathak14@users.noreply.github.com>
2023-02-27 12:11:51 +0530

    Update Teacher

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+
+
 public class Teacher {
 
     private String name;
@@ -7,8 +9,39 @@ public class Teacher {
     private int numberOfStudents;
 
     private int age;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public Teacher() {
 
     }
+
+    public Teacher(String name, int numberOfStudents, int age) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
+    }
+
+    
 }
